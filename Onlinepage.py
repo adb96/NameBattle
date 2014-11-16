bca4bfc4a4ceb3bb2aac88bd2c9ff29b7e0b6c46
mekemeke4 <mts51@pitt.edu>
2014-11-16 16:26:29 -0500

    timechange1

--- Onlinepage.py
+++ Onlinepage.py
@@ -107,7 +107,12 @@ class UserHandler(webapp2.RequestHandler):
 		users = UserInfo.query().fetch()
 		tmp=""
 		for user in users:
-			if (user.date - datetime.now()) < 0 && (user.date - datetime.now()) > -1:
+			current = datetime.now()
+			last = user.date
+			time_dif = (current-last).total_seconds()
+			if time_dif > 1*60:
+				pass
+			else:
 				tmp+='<li class="useritem">'+user.user+'</li>\n'   #user has been on within a minute. Print that they're online
 				
 
