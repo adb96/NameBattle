b65403c8caa51f7ce1c89d22174e8a16a8353e02
adb96 <adb96@pitt.edu>
2014-12-03 18:25:13 -0500

    Empty How to Play edit

--- js/howtoplay.js
+++ js/howtoplay.js
@@ -9,7 +9,12 @@ function displayInfo(){
 	document.getElementById("loginmain2").style.opacity="0.2";
 
   var displayMsg="";
-  
+	displayMsg+="<h3>Basic Options</h3>";
+	displayMsg+="<ol><li>First time players should choose Create Role to created a character to play with</li>";
+	displayMsg+="<li>From there, they can select one of their roles to see that role's randomly generated stats</li>";
+	displayMsg+="<li>Then they can enter a name in the other slot of the game page for an on-line fight</li>";
+	displayMsg+="<li>Or they can go back to the title screen and choose Who's Online</li>";
+	displayMsg+="<li>Here they can see other player's online, and queue up for a random fight</li>";
   
   //function to set the created info to the hidden div pop-up
 	document.getElementById("msg1").children[0].innerHTML=displayMsg;
